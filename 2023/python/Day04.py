20247c2a052f685b467bffefcb142a1685b20de3
korigamik-hypr <korigamik@gmail.com>
2023-12-13 23:36:13 +0530

    finish part 2

--- 2023/python/Day04.py
+++ 2023/python/Day04.py
@@ -1,3 +1,6 @@
+from functools import reduce
+
+
 def part1(lines: list[str]):
     sum_ = 0
     for line in lines:
@@ -15,14 +18,23 @@ def part1(lines: list[str]):
 
 
 def part2(lines: list[str]):
+    got_instances: list[int] = list(0 for _ in range(len(lines)))
     for line in lines:
         card, rest = line.split(": ")
         card = int(card[4:])
         winning, have = map(lambda x: map(int, x.split()), rest.split(" | "))
-        print()
+        winners = set(winning)
+        got = reduce(lambda acc, x: acc + 1 if x in winners else acc, have, 0)
+        got_instances[card - 1] = got
+
+    score = [1] * len(got_instances)
+    for c, wins in enumerate(got_instances):
+        i, j = c + 1, c + 1 + wins
+        score[i:j] = [x + score[c] for x in score[i:j]]
+    print(sum(score))
 
 
 if __name__ == "__main__":
     lines = open(0).read().strip().split("\n")
-    part1(lines)
+    # part1(lines)
     part2(lines)
